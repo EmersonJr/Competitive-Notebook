68c67f2c66ab41d1955fca3839d984874e565e1d
Emerson Junior <emersonjr02@gmail.com>
2024-06-21 19:50:24 -0300

    Update Template.cpp

--- C++/Misc/Template.cpp
+++ C++/Misc/Template.cpp
@@ -1,22 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 #define sws std::ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-#define int long long
-#define endl "\n"
+#define int long long int
+#define float long double
+#define ld long double
+#define ll long long
 #define pb push_back
-#define all(x) x.begin(), x.end()
 #define ff first
 #define ss second
+#define vi vector<int>
+#define vpii vector<pair<int, int>> 
+#define vvi vector<vector<int>>
+#define pii pair<int, int>
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define in(v) for(auto & x : v) cin >> x;
+#define out(v) for(auto x : v) cout << x << ' ';
+#define tfii tuple<float, int, int>
 
-typedef long long ll;
-typedef long double ld;
-typedef vector<int> vi;
-typedef pair<int,int> pii;
-typedef priority_queue<int, vector<int>, greater<int>> pqi;
+const int MAXN = 31700;
+const int INF = INT64_MAX;
+const int MOD = 1e9+7;
+const int LOG = 31;
+const ld PI = acos(-1);
+const int MINF = INT64_MIN;
+vpii dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
-const ll MOD = 1e9+7;
-const int MAX = 1e4+5;
-const ll LLINF = 0x3f3f3f3f3f3f3f3f;
-const double PI = acos(-1);
+void solve(){
+
+
+    return;
+}
+int32_t main(){ 
+    sws;
+
+    int t = 1;
+    // cin >> t;
+    
+    while(t--)
+        solve();
+
+    return 0;
+}
